1d8493344da7121591df655a855d5a4df37722bf
El_Dan <lilit-li@ya.ru>
2022-09-08 21:17:30 +0400

    Изменена Задача 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -7,15 +7,16 @@ b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 Console.Clear();
 
 Console.WriteLine("Введите значение b1: ");
-int b1 = Convert.ToInt32(Console.ReadLine());
+double b1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите значение b2: ");
-int b2 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите значение k1: ");
-int k1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите значение k2: ");
-int k2 = Convert.ToInt32(Console.ReadLine());
-
- int x = (b2 - b1) / (k1 - k2);
- int y = k1 * x + b1;
+double k2 = Convert.ToInt32(Console.ReadLine());
+if (b1 == b2 & k1 == k2)
+    Console.WriteLine("Прямые совпадают");
+ double x = (b2 - b1) / (k1 - k2);
+ double y = k1 * x + b1;
 
 Console.WriteLine("Координаты пересечения двух прямых: " + "x: " + x + " y: " + y);
